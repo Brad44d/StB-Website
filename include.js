f48f6aa79b00e18f3366f06dd43de1c0f7e8bb45
Brad44d <232219178+Brad44d@users.noreply.github.com>
2026-06-21 11:23:25 +0200

    Design-Polish: Navigation, Hover-Effekte, Abstände, Google Maps

--- include.js
+++ include.js
@@ -1,5 +1,17 @@
-function loadHTML(id, file) {
+function loadHTML(id, file, callback) {
     fetch(file)
         .then(response => response.text())
-        .then(data => document.getElementById(id).innerHTML = data);
+        .then(data => {
+            document.getElementById(id).innerHTML = data;
+            if (callback) callback();
+        });
+}
+
+function highlightNav() {
+    const currentPage = location.pathname.split("/").pop() || "index.html";
+    document.querySelectorAll("nav a").forEach(link => {
+        if (link.getAttribute("href") === currentPage) {
+            link.classList.add("active");
+        }
+    });
 }
